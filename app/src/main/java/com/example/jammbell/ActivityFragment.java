20883326cb9760fc8ddb68645adf326dc217f8be
Paolo Di Stasio <paolo.distasio98@gmail.com>
2021-06-25 19:37:50 +0200

    boh

--- app/src/main/java/com/example/jammbell/ActivityFragment.java
+++ app/src/main/java/com/example/jammbell/ActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -76,6 +77,8 @@ public class ActivityFragment extends Fragment {
         recyclerViewStorico = (RecyclerView) getView().findViewById(R.id.recyclerViewStorico);
 
         setHasOptionsMenu(true);
+
+
 
         dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
